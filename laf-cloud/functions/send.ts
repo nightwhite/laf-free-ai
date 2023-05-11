d31b3240a3ce25bc233f055c536dd050a79fcb8d
NightWhite <nightwhite634@gmail.com>
2023-05-11 15:10:52 +0800

    修改上下文的云函数

--- laf-cloud/functions/send.ts
+++ laf-cloud/functions/send.ts
@@ -2,12 +2,20 @@ import cloud from '@lafjs/cloud'
 const inviteCode = "" // 引号里面填入你的邀请码
 
 export async function main(ctx: FunctionContext) {
-  const { question } = ctx.body
-  const res = await cloud.fetch.post("https://htr4n1.laf.run/ai-api",{
-    question,
-    inviteCode
-  })
-  return res.data
+  console.log(ctx.body)
+  const { question, parentMessageId } = ctx.body
+  if (parentMessageId){
+    const res = await cloud.fetch.post("https://htr4n1.laf.run/ap-api-context", {
+      question,
+      inviteCode,
+      parentMessageId
+    })
+    return res.data
+  }else{
+    const res = await cloud.fetch.post("https://htr4n1.laf.run/ap-api-context", {
+      question,
+      inviteCode
+    })
+    return res.data
+  }
 }
-
-
